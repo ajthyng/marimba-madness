a41a05230190ee923931735cf495ba302d2ad691
Andrew Thyng <andrew@thyng.dev>
2021-03-20 10:35:54 -0500

    Fixed build problems with tone import

--- src/synth/Synth.js
+++ src/synth/Synth.js
@@ -1,40 +1,40 @@
-import { scale } from '../constants/scale'
-import Tone from 'tone'
+import { scale } from "../constants/scale";
+import * as Tone from "tone";
 
 const getTransport = () => {
-  return Tone.Transport
-}
+  return Tone.Transport;
+};
 
 class Synth {
-  constructor (callback, samples = 'audio/marimba/') {
-    this.sampler = new Tone.Sampler(scale, callback, samples)
-    this.sampler.toMaster()
-    this.setVolume()
+  constructor(callback, samples = "audio/marimba/") {
+    this.sampler = new Tone.Sampler(scale, callback, samples);
+    this.sampler.toMaster();
+    this.setVolume();
   }
 
-  setVolume (volume = -12) {
-    this.sampler.volume.value = volume
+  setVolume(volume = -12) {
+    this.sampler.volume.value = volume;
   }
 
-  toggle () {
-    if (Tone.context.state !== 'running') {
-      Tone.context.resume()
+  toggle() {
+    if (Tone.context.state !== "running") {
+      Tone.context.resume();
     }
-    getTransport().toggle()
+    getTransport().toggle();
   }
 
-  repeat (callback, timing = '8n') {
-    this.timing = timing
-    getTransport().scheduleRepeat(callback, timing)
+  repeat(callback, timing = "8n") {
+    this.timing = timing;
+    getTransport().scheduleRepeat(callback, timing);
   }
 
-  playNote (note, time, timing = '8n') {
-    this.sampler.triggerAttackRelease(note, this.timing || timing, time)
+  playNote(note, time, timing = "8n") {
+    this.sampler.triggerAttackRelease(note, this.timing || timing, time);
   }
 
-  setBPM (bpm = 120) {
-    getTransport().bpm.value = bpm
+  setBPM(bpm = 120) {
+    getTransport().bpm.value = bpm;
   }
 }
 
-export default Synth
+export default Synth;
