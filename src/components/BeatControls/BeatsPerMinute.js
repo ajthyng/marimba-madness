bc67d60d1c35aed0079629c06a33944a6ccaaedc
Andrew Thyng <andrew@thyng.dev>
2021-03-23 00:11:09 -0500

    Rename Beats Per Minute to BPM

--- src/components/BeatControls/BeatsPerMinute.js
+++ src/components/BeatControls/BeatsPerMinute.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import RangeInput from './RangeInput'
+import React, { Component } from "react";
+import styled from "styled-components";
+import RangeInput from "./RangeInput";
 
 const Container = styled.div`
   margin-left: 8px;
@@ -9,67 +9,67 @@ const Container = styled.div`
   align-items: center;
   justify-content: flex-start;
   flex-direction: column;
-`
+`;
 
 const Label = styled.span`
   color: white;
   font-size: 16px;
   font-weight: 700;
   width: 100%;
-`
+`;
 
 class BeatsPerMinute extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      bpm: this.props.bpm || 160
-    }
+      bpm: this.props.bpm || 160,
+    };
 
-    this.input = React.createRef()
+    this.input = React.createRef();
   }
-  componentDidMount () {
-    const { current: input } = this.input
-    input.value = this.state.bpm
-    this.addEventListeners(this.handleChange)
-  }
-
-  componentWillUnmount () {
-    const { current: input } = this.input
-    input.value = this.state.bpm
-    this.removeEventListeners(this.handleChange)
+  componentDidMount() {
+    const { current: input } = this.input;
+    input.value = this.state.bpm;
+    this.addEventListeners(this.handleChange);
   }
 
-  handleChange = event => {
-    const { handleChange } = this.props
-    this.setState({ bpm: event.target.value })
-    handleChange(event)
+  componentWillUnmount() {
+    const { current: input } = this.input;
+    input.value = this.state.bpm;
+    this.removeEventListeners(this.handleChange);
   }
 
-  addEventListeners (handleChange) {
-    const { current: input } = this.input
-    input.addEventListener('input', handleChange)
-    input.addEventListener('change', handleChange)
+  handleChange = (event) => {
+    const { handleChange } = this.props;
+    this.setState({ bpm: event.target.value });
+    handleChange(event);
+  };
+
+  addEventListeners(handleChange) {
+    const { current: input } = this.input;
+    input.addEventListener("input", handleChange);
+    input.addEventListener("change", handleChange);
   }
 
-  removeEventListeners (handleChange) {
-    const { current: input } = this.input
-    input.removeEventListener('input', handleChange)
-    input.removeEventListener('change', handleChange)
+  removeEventListeners(handleChange) {
+    const { current: input } = this.input;
+    input.removeEventListener("input", handleChange);
+    input.removeEventListener("change", handleChange);
   }
 
-  render () {
-    const { bpm } = this.state
+  render() {
+    const { bpm } = this.state;
     return (
       <Container>
-        <Label>{`Beats Per Minute - ${bpm}`}</Label>
-        <RangeInput ref={this.input} type='range' min='80' max='200' />
+        <Label>{`BPM - ${bpm}`}</Label>
+        <RangeInput ref={this.input} type="range" min="80" max="200" />
       </Container>
-    )
+    );
   }
 }
 
 BeatsPerMinute.defaultProps = {
-  handleChange: () => null
-}
+  handleChange: () => null,
+};
 
-export default BeatsPerMinute
+export default BeatsPerMinute;
