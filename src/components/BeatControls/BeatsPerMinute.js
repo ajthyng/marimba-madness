326aa6413833bde7507abe507a869b1b3b8b446d
Andrew Thyng <andrew@thyng.dev>
2021-03-23 22:56:26 -0500

    Going back to original Beats Per Minute

--- src/components/BeatControls/BeatsPerMinute.js
+++ src/components/BeatControls/BeatsPerMinute.js
@@ -61,7 +61,7 @@ class BeatsPerMinute extends Component {
     const { bpm } = this.state;
     return (
       <Container>
-        <Label>{`BPM - ${bpm}`}</Label>
+        <Label>{`Beats Per Minute - ${bpm}`}</Label>
         <RangeInput ref={this.input} type="range" min="80" max="200" />
       </Container>
     );
